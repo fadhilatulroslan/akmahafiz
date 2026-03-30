848dac41ce460a586df8b904188d3100d9dd185c
fadhilatulroslan <fadhilatulakma3@gmail.com>
2026-03-31 00:59:15 +0800

    Add files via upload

---
+++ addtocart.js
@@ -0,0 +1,106 @@
+let data = {};
+let hasil = 0;
+
+document.addEventListener('DOMContentLoaded', function() {
+    window.troli = document.querySelector("#troli");
+});
+
+function tambahData(dapatkanBarang) {
+    if (dapatkanBarang.nama in data) {
+        data[dapatkanBarang.nama].jumlah++;
+    } else {
+        data[dapatkanBarang.nama] = {
+            harga: dapatkanBarang.harga,
+            jumlah: 1
+        };
+    }
+
+    kiraHasil();
+    renderTroli();
+    console.log(data);
+    console.log("Jumlah: RM" + hasil.toFixed(2));
+    return data;
+}
+
+function kiraHasil() {
+    hasil = 0;
+    for (let key in data) {
+        hasil += data[key].harga * data[key].jumlah;
+    }
+}
+
+function renderTroli() {
+    let troli = document.querySelector("#troli");
+    if (!troli) {
+        console.error("Element #troli tak jumpa!");
+        return;
+    }
+    
+    kosongkanElement(troli);
+    
+    for (let key in data) {
+        // Container untuk satu item
+        let itemContainer = document.createElement("div");
+        itemContainer.classList.add("kandungan-kedua");
+        
+        let divBarang = document.createElement("div");
+        divBarang.textContent = `${key} (${data[key].jumlah}x)`;
+        
+        let divButton = document.createElement("div");
+        let button = document.createElement("button");
+        button.classList.add("buang");
+        
+        let icon = document.createElement("i");
+        icon.classList.add("fa-solid", "fa-trash-can");
+        
+        let textNode = document.createTextNode(" Buang");
+        
+        button.appendChild(icon);
+        button.appendChild(textNode);
+        
+        button.onclick = function() {
+            buangItem(key);
+        };
+        
+        divButton.appendChild(button);
+        
+        itemContainer.appendChild(divBarang);
+        itemContainer.appendChild(divButton);
+        
+        troli.appendChild(itemContainer);
+    }
+    
+    // JUMLAH
+    let jumlahContainer = document.createElement("div");
+    jumlahContainer.classList.add("jumlah");
+    
+    let pJumlah = document.createElement("p");
+    pJumlah.textContent = "Jumlah";
+    
+    let pTotal = document.createElement("p");
+    pTotal.classList.add("bold");
+    pTotal.textContent = "RM" + hasil.toFixed(2);
+    
+    jumlahContainer.appendChild(pJumlah);
+    jumlahContainer.appendChild(pTotal);
+    
+    troli.appendChild(jumlahContainer);
+}
+
+function kosongkanElement(elemen) {
+    while (elemen.firstChild) {
+        elemen.removeChild(elemen.firstChild);
+    }
+}
+
+function buangItem(nama) {
+    if (data[nama]) {
+        if (data[nama].jumlah > 1) {
+            data[nama].jumlah--;
+        } else {
+            delete data[nama];
+        }
+        kiraHasil();
+        renderTroli();
+    }
+}
